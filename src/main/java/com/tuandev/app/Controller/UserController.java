d751b18f6fb1e2586c9f58b07d419654f87c8803
tuanpa06 <phamtuananh08082006@gmail.com>
2025-07-15 21:44:33 +0700

    Implement api update user by Id

--- src/main/java/com/tuandev/app/Controller/UserController.java
+++ src/main/java/com/tuandev/app/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.tuandev.app.Controller;
 
 import com.tuandev.app.Dto.Request.CreateUserRequest;
+import com.tuandev.app.Dto.Request.UpdateUserRequest;
 import com.tuandev.app.Entity.User;
 import com.tuandev.app.Service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,12 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest){
         User user = userService.create(createUserRequest);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody UpdateUserRequest updateUserRequest){
+        User user = userService.update(id, updateUserRequest);
         return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/tuandev/app/Service/UserService.java
+++ src/main/java/com/tuandev/app/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.tuandev.app.Service;
 
 import com.tuandev.app.Dto.Request.CreateUserRequest;
+import com.tuandev.app.Dto.Request.UpdateUserRequest;
 import com.tuandev.app.Entity.User;
 import com.tuandev.app.Mapper.UserMapper;
 import com.tuandev.app.Repository.UserRepository;
@@ -21,6 +22,19 @@ public class UserService {
     public User create(CreateUserRequest createUserRequest){
         User user = userMapper.toUser(createUserRequest);
         user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User update(int id, UpdateUserRequest updateUserRequest){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+
+        user.setPassword(passwordEncoder.encode(updateUserRequest.getPassword()));
+        user.setEmail(updateUserRequest.getEmail());
+        user.setGender(updateUserRequest.getGender());
+        user.setPhoneNumber(updateUserRequest.getPhoneNumber());
+        user.setBod(updateUserRequest.getBod());
+
         return userRepository.save(user);
     }
 }
